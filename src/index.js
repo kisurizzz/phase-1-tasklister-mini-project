933ed8171f955214cf0d69689882525b9d93684d
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-04-02 15:16:37 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener('DOMContentLoaded', () => {
+
+  
+  
+//   let btn = document.getElementById('btn')
+//   btn.addEventListener('click', () => {
+//       btn.textContent = 'Ordered placed'
+//       alert('Order placed successfully')
+//   })
+//   let form = document.querySelector('form')
+//   let input = document.getElementById('text')
+  
+//   form.addEventListener('submit', (event) => {
+//       event.preventDefault()
+//       let par = document.createElement('p')
+//       par.textContent = input.value
+//       body.appendChild(par)
+//       input.value = ''
+//   })
+  
+//   })
+  
+
+  document.addEventListener('DOMContentLoaded',()=>{
+    let form =document.querySelector('form')
+    form.addEventListener('submit',(event)=>{
+      event.preventDefault()
+    //  console.log(event.target.newtask.value)
+    //  console.log(event.target.user.value)
+      let newTodo=event.target.newtask.value
+      let user=event.target.user.value
+      let priority=event.target.priority.value
+      console.log(priority)
+      Todo(newTodo,user,priority)
+      console.log(newTodo,user)
+      form.reset()
+    })
+  })
+  
+  function Todo(todo,user2,prioritY){
+  const li=document.createElement('li')
+  const btn=document.createElement('button')
+  const user=document.createElement('span')
+  btn.addEventListener('click',handleDelete)
+  li.addEventListener('mouseover',setColor)
+  btn.innerText='x'
+  li.innerHTML=`${todo} ${user2} ${prioritY} 
+   `
+   
+  li.appendChild(btn)
+  
+  
+  function setColor(prioritY){
+    if(prioritY==='Urgent'){
+     prioritY=document.getElementsByTagName('li').style.color = "#cccfffggg";
+    }else if(prioritY==='Less Urgent'){
+     prioritY= document.getElementsByTagName('li').style.color = "green";
+    }
+    return prioritY
+  }
+  setColor()
+  document.querySelector('#tasks').appendChild(li)
+  }
+  
+  function handleDelete(e){
+    e.target.parentNode.remove()
+  }
